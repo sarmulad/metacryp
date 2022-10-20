34de90ec2797dcca061439b2de7fcaa54ce688c8
sarmulad <salammulad@gmail.com>
2022-10-20 17:41:11 +0100

    first commit

--- src/components/Home/faq/Faq.jsx
+++ src/components/Home/faq/Faq.jsx
@@ -12,21 +12,14 @@ const faqData = [
         title:'2. Why was MetaCryp Made',
         content:'The aim of MetaCryp is to create a home away from home, offering people a space to have intense fun and forget about the stress of daily living.'
      },
+     
      {
-        title:'3. Will there be a Whitelisted Presale ',
-        content:'Yes, there will be.'
-     },
-     {
-        title:'4. Who makes decision in the MetaCryp Network',
+        title:'3. Who makes decision in the MetaCryp Network',
         content:'MetaCryp Network is a community-centric ecosystem, meaning that holders of the native tokens can submit proposals and vote on changes'
      },
      {
-        title:'5. Will there be NFTs',
+        title:'4. Will there be NFTs',
         content:'Yes, there will be.'
-     },
-     {
-        title:'6. What’s will be the Mint Price',
-        content:'The mint price will be dependent on how early you interact with the community. Earlly users will have access to a cheaper mint price when the public mint begins'
      },
    
   ];
